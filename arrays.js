262a27b16184db112fde55e5fea55c31ef9d2e6a
Rafael Lopez <lpz.raf@gmail.com>
2018-05-21 00:41:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"];
+
+
+
+function addElementToBeginningOfArray(array, element) { element = [element]; const array = [...element, array];
+  return array;
+}
+
+
+
